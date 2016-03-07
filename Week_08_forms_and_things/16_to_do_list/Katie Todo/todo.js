3e18cf16452dcb0039dd923dfed483ece9317b88
katiecampbell <katiec@trilliumpublishing.com>
2016-03-06 20:33:39 -0800

    To-do List homework

--- Week_08_forms_and_things/16_to_do_list/Katie Todo/todo.js
+++ Week_08_forms_and_things/16_to_do_list/Katie Todo/todo.js
@@ -3,19 +3,24 @@ $(document).ready(function(){
 
 //add a todo item to the list
 	$("#item_submit").submit(function( event ) {
-  		alert( "Handler for .submit() called." );
+ //  		alert( "Handler for .submit() called." );
   		event.preventDefault();
 
   		var todo = $("#itemText").val();
   		console.log(todo);
 
-  		$("#item_list").append("<li>" + todo + "<a href='#' class='deleteItem'>x</a></li>");
+  		$("#item_list").append("<li>" + "<a href='#' class='checkmark'>&#10004;</a>" + todo + "<a href='#' class='deleteItem'>x</a></li>");
+    });
 
-  		
-	});
-	//Hover does not work
-	// $("#item_list").on("hover", "li", function() {
-	// 	debugger;
- //  		$("#deleteButton").show();
-  	// });
+  $("#item_list").on('click', '.deleteItem', function(){
+    console.log('event is happening?');
+    $(this).parent('li').remove();
+  });
+
+  $("#item_list").on('click', '.checkmark', function(){
+    console.log('checkmark clicked');
+    $(this).parent('li').css('color', "green");
+  });
 });
+
+
